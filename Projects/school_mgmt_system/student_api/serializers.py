6b661f5bcc6ec61bda06ee22cd5d7b2d3ff1cdb7
Laxman Maharjan <lxmnmrzn@gmail.com>
2022-07-11 21:08:53 +0545

    writable nested objects

--- Projects/school_mgmt_system/student_api/serializers.py
+++ Projects/school_mgmt_system/student_api/serializers.py
@@ -1,8 +1,57 @@
 from rest_framework import serializers
+from student_api.utils import College, Student
+
+class CollegeSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=30)
+    location = serializers.CharField(max_length=30)
+
+    def create(self, validated_data):
+        return College(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.location = validated_data.get('location', instance.location)
+        return instance
 
 class StudentSerializer(serializers.Serializer):
     roll_no = serializers.CharField(max_length=20)
     name = serializers.CharField(max_length=30)
     email = serializers.EmailField()
     age = serializers.IntegerField()
-    created_at = serializers.DateTimeField()
+    college = CollegeSerializer()
+
+    def create(self, validated_data):
+        serializer = CollegeSerializer(data = validated_data['college'])
+        college = None
+        if serializer.is_valid():
+            college = serializer.save()
+
+        validated_data['college'] = college
+        return Student(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.roll_no = validated_data.get('roll_no', instance.roll_no)
+        instance.name = validated_data.get('name', instance.name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.age = validated_data.get('age', instance.age)
+
+        serializer = CollegeSerializer(instance.college, data = validated_data['college'])
+        college = None
+        if serializer.is_valid():
+            college = serializer.save()
+
+        instance.college = college
+        return instance
+
+    def validate_age(self, value):
+        if value < 18:
+            raise serializers.ValidationError("You are Under Age. Must be 18+")
+        return value
+
+    def validate(self, data):
+        if data['name'] == 'ram' and data['age'] < 20:
+            raise serializers.ValidationError("If you are Ram you must be 20+")
+        return data
+
+
+
--- Projects/school_mgmt_system/student_api/utils.py
+++ Projects/school_mgmt_system/student_api/utils.py
@@ -1,13 +1,28 @@
 from datetime import datetime
 
+class College:
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"{self.name}:{self.location}"
+
 class Student:
-    def __init__(self, roll_no, name, email, age):
+    def __init__(self, roll_no, name, email, age, college):
         self.roll_no = roll_no
         self.name = name
         self.email = email
         self.age = age
         self.created_at = datetime.now()
+        self.college = college
 
     def __str__(self):
         return f"<Student: {self.name}>"
+
+    def __repr__(self) -> str:
+        return f"<Student: {self.name}:{self.age}>"
 
